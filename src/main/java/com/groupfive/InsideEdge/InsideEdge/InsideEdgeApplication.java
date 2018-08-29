124f6dfa27793889e050c97cdc3aa13e62b5bb95
M <sneha.a.m@capgemini.com>
2018-08-29 13:07:05 +0530

    FirstCommit

---
+++ src/main/java/com/groupfive/InsideEdge/InsideEdge/InsideEdgeApplication.java
@@ -0,0 +1,12 @@
+package com.groupfive.InsideEdge.InsideEdge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideEdgeApplication.class, args);
+	}
+}
